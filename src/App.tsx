1712c63a2fae07db44ab3074d4aa8ca137d9de7c
tanishkapatidar <tanishkapatidar18@gmail.com>
2025-02-19 08:42:32 +0530

    Integrated React Router with Counter, UserForm, RichTextEditor, and Dashboard in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Counter from "./components/Counter";
+import UserForm from "./components/UserForm";
+import RichTextEditor from "./components/RichTextEditor";
+import Dashboard from "./components/Dashboard";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { Box, Button } from "@mui/material";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Box sx={{ textAlign: "center", margin: "20px" }}>
+          <Button component={Link} to="/">Counter</Button>
+          <Button component={Link} to="/form">User Form</Button>
+          <Button component={Link} to="/editor">Rich Text Editor</Button>
+          <Button component={Link} to="/dashboard">Dashboard</Button>
+        </Box>
+
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path="/form" element={<UserForm />} />
+          <Route path="/editor" element={<RichTextEditor />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
